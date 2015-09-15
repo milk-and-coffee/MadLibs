60654808335731f0db7bd9e6df62fb8155ca18af
milk-and-coffee <milk-and-coffee@users.noreply.github.com>
2015-09-15 16:09:57 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,7 @@
 #! python3
 # madlibs.py - Reads a madlibs text file and prompts the user to input
 #              appropriate words. Allows the user to save the final
-#              story as its a separate text file.
+#              story as a separate text file.
 
 import os
 import re
