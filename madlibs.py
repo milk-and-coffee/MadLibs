af874c2869139748275af1b6baf8496ba4554ddf
Jeff L <jeffclura@gmail.com>
2015-09-17 19:07:24 -0400

    Adding edited/improved version of madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,51 +1,49 @@
 #! python3
 # madlibs.py - Reads a madlibs text file and prompts the user to input
 #              appropriate words. Allows the user to save the final
-#              story as its a separate text file.
-
+#              story as a separate text file.
+ 
 import os
 import re
-
+ 
 def createStory():
     '''Creates a new story based on chosen template and optionally
-    saves the result to the Stories folder'''
+   saves the result to the Stories folder'''
     # Display possible story templates and allow user to choose one
-    templatePath = os.path.join(os.getcwd(), 'Templates')
     print('Choose a template:')
-    for file in os.listdir(templatePath):
+    for file in os.listdir('Templates'):
         print(file)
     while True:
         selection = input('> ')
-        if selection in os.listdir(templatePath):
-            story = open(os.path.join(templatePath, selection))
+        if selection in os.listdir('Templates'):
+            with open(os.path.join('Templates', selection)) as story:
+                storyContent = story.read()
             break
         else:
             print('Invalid selection')
-
+ 
     # Create a list of all instances from story template in need of replacing
-    storyContent = story.read()
     madlibsRegex = re.compile(r'NOUN\d+|PERSON\d+|CITY\d+|VERB\d+|ADJECTIVE\d+')
     mo = madlibsRegex.findall(storyContent)
-
+ 
     # Remove duplicates
-    subs = []
-    for word in mo:
-        if not word in subs:
-            subs.append(word)
-
+    subs = set(mo)
+ 
     # Prompt user for replacement for each instance
     for word in subs:
-        print('Enter a ' + word[:-1])
+        print('Enter a ' + word.rstrip('0123456789'))
         entry = input('> ')
         storyContent = re.sub(word, entry, storyContent)
-
+ 
     # Display finished story and prompt user to save
     # If 'yes' save story in 'stories' folder
     print(storyContent)
     print('Would you like to save this story?')
     res = input('> ')
-
+ 
     if res == 'y' or res == 'yes':
+        if not os.path.exists('Stories'):
+            os.makedirs('Stories')
         print('Enter a name for your story: ')
         title = input('> ')
         location = os.path.join(os.getcwd(), 'stories', title + '.txt')
@@ -53,7 +51,7 @@ def createStory():
         savedStory.write(storyContent)
         savedStory.close()
         print('Story saved!')
-
+ 
 def readStory():
     '''Displays list of saved stories and prints chosen story'''
     storyPath = os.path.join(os.getcwd(), 'Stories')
@@ -70,7 +68,7 @@ def readStory():
     storyContent = story.read()
     print(storyContent)
     story.close()
-
+ 
 def mainMenu():
     print('Welcome to Mad Libs!\n')
     while True:
@@ -85,6 +83,6 @@ def mainMenu():
             break
         else:
             print('Invalid selection')
-
+ 
 if __name__ == '__main__':
     mainMenu()
